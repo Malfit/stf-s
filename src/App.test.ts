07690c1a36cedfd6bbee8298189b4ebc3ae21e66
c-oleksandr-rybakov <c-oleksandr.rybakov@hulu.com>
2024-11-27 23:30:26 +0200

    feat: add test example

---
+++ src/App.test.ts
@@ -0,0 +1,9 @@
+describe('Password Validation', () => {
+  it('should validate a correct password', async () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('should validate a correct password', async () => {
+    expect(false).toBeTruthy();
+  });
+})
